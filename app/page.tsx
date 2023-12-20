93d072c1197c2af306efc65b3a93e09f890edeb5
Samuel Souza <samuel_f2@icloud.com>
2023-12-20 17:49:00 -0300

    fix: typescript error

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className="h-[100dvh] bg-[#f9f9f8]">
       <nav className="flex text-white items-center p-4 justify-end bg-neutral-900 w-full h-[68px]">
-        <LogOut className='cursor-pointer' onClick={signOut}/>
+        <LogOut className='cursor-pointer' onClick={() => signOut()}/>
       </nav>
       <section className="w-full grid grid-cols-1 sm:grid-cols-2 sm:gap-4 lg:grid-cols-4 py-4">
         <Deck title="Amazon Web Services" imgSrc={AWS} />
