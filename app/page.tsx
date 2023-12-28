cabecdc72a91310ba4f3e66ae3b83ed7bbd659ba
samuel <samuel@convestconsultoria.com.br>
2023-12-28 20:22:03 +0000

    fix: corrige uso de session

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,6 @@ async function getBaralhos() {
 }
 
 export default async function Home() {
-  const { data: session } = useSession()
   const baralhos = await getBaralhos()
 
   return (
@@ -16,7 +15,6 @@ export default async function Home() {
       <nav className="flex text-white items-center p-4 justify-end bg-neutral-900 w-full h-[68px]">
         <LogOutButton/>
       </nav>
-      {session?.user?.email}
       <section className="w-full grid grid-cols-1 gap-8 sm:grid-cols-2 sm:gap-4 lg:grid-cols-4 py-4">
         {baralhos.map((baralho, i) => {
           return <Baralho key={i} baralho={baralho}/>
