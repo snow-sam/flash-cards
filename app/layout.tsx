09318611c8fe4e7f1e61b0e8cb25b85e202e7b1f
Samuel Souza <samuel_f2@icloud.com>
2024-01-12 01:21:59 -0300

    ref: ajusta no layout o erro de zoom em input do IOS

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,7 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
       <body className={inter.className}>
         {children}
         <Toaster/>
