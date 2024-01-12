c76e50616dddaac1ecbdcdd165db7d2d2bc05af9
Samuel Souza <samuel_f2@icloud.com>
2024-01-12 01:41:27 -0300

    hotfix: remove uso de api interna devido a erros em produção

--- app/cartas/[id]/page.tsx
+++ app/cartas/[id]/page.tsx
@@ -5,24 +5,6 @@ import { headers } from 'next/headers'
 
 
 async function getCards(deckId: string) {
-  const headersData = headers()
-  const host = headersData.get('host') ?? ''
-  const protocol = headersData.get('x-forwarded-proto') ?? host.startsWith('localhost') ? 'http' : 'https' 
-  const apiBase = `${protocol}://${host}`
-
-  const session = await auth()
-  fetch(`${apiBase}/api/usersOnDecks`, {
-    method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      userId: session?.user.id,
-      deckId: deckId,
-      lastViewAt: new Date(),
-    }),
-  });
-
   return await prisma.card.findMany({
     where: {
       deckId
